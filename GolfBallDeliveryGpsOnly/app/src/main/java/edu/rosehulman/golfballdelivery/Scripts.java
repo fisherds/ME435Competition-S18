607ebca9b1cdbf47ef8f889eb48987c2bb373a04
David Fisher <fisherds@rose-hulman.edu>
2018-04-30 13:52:17 -0400

    Start a Cone Finder app

--- GolfBallDeliveryGpsOnly/app/src/main/java/edu/rosehulman/golfballdelivery/Scripts.java
+++ GolfBallDeliveryGpsOnly/app/src/main/java/edu/rosehulman/golfballdelivery/Scripts.java
@@ -35,7 +35,9 @@ public class Scripts {
     double distanceToNearBall = NavUtils.getDistance(15, 0, 90, 50);
     long driveTimeMs = (long) (distanceToNearBall / RobotActivity.DEFAULT_SPEED_FT_PER_SEC * 1000);
 
-    // TODO: FOR testing make it shorter!
+    // For testing this has been made shorter!
+    driveTimeMs = 3000;
+
     mActivity.sendWheelSpeed(mActivity.mLeftStraightPwmValue, mActivity.mRightStraightPwmValue);
     mCommandHandler.postDelayed(new Runnable() {
       @Override
@@ -58,7 +60,23 @@ public class Scripts {
   }
 
   public void farBallScript() {
-    // TODO: Implement
+    mActivity.sendWheelSpeed(0, 0);
+    removeBallAtLocation(mActivity.mFarBallLocation);
+
+    mCommandHandler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        mActivity.sendWheelSpeed(0,0);
+
+        if (mActivity.mWhiteBallLocation != 0) {
+          removeBallAtLocation(mActivity.mWhiteBallLocation);
+        }
+        if (mActivity.mState == GolfBallDeliveryActivity.State.FAR_BALL_SCRIPT) {
+          mActivity.setState(GolfBallDeliveryActivity.State.DRIVE_TOWARDS_HOME);
+        }
+
+      }
+    }, ARM_REMOVAL_TIME);
   }
 
 
